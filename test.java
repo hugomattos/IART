6291e672830321d0ad58d9da8b656ea41b3004c2
Hugo Matos <hugomattos15@gmail.com>
2015-06-04 14:52:02 +0100

    final

--- test.java
+++ test.java
@@ -1,41 +1,55 @@
-import weka.classifiers.Classifier;
+import java.io.PrintWriter;
+import java.io.Writer;
+
 import weka.classifiers.Evaluation;
 import weka.classifiers.functions.MultilayerPerceptron;
-import weka.core.Attribute;
-import weka.core.FastVector;
-import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
  
 public class test {
  
     public static void main(String[] args) throws Exception{
-    	
-    	 DataSource source = new DataSource("teste.arff");
-    	 Instances data = source.getDataSet();
-    	 if (data.classIndex() == -1)
-    		   data.setClassIndex(data.numAttributes() - 1);
+        PrintWriter writer = new PrintWriter("log.txt", "UTF-8");
+
+    	 //train
+    	 DataSource source1 = new DataSource("train.arff");
+    	 Instances train = source1.getDataSet();
+    	 if (train.classIndex() == -1)
+    		   train.setClassIndex(train.numAttributes() - 1);
+    	 //test
+    	 DataSource source2 = new DataSource("teste.arff");
+    	 Instances test = source2.getDataSet();
+    	 if (test.classIndex() == -1)
+    		   test.setClassIndex(test.numAttributes() - 1);
     	 
+    	 //modelo que executa o backpropagation
          MultilayerPerceptron cModel = new MultilayerPerceptron(); 
-         cModel.buildClassifier(data);
          
+         // atributos + classes * 2/3
+         cModel.setHiddenLayers("67");
          
-         // Test the model
-         Evaluation eTest = new Evaluation(data);
-         eTest.evaluateModel(cModel,data);
-          
-         // Print the result à la Weka explorer:
+         cModel.setGUI(true);
+         cModel.buildClassifier(train);
+         
+         // testar o modelo
+         Evaluation eTest = new Evaluation(test);
+         eTest.evaluateModel(cModel,test);
+         
+         // print dos resultados
          String strSummary = eTest.toSummaryString();
-         System.out.println(strSummary);
-          
-         // Get the confusion matrix
+         writer.print("" + strSummary.substring(0, 135) + "\n");
+
+         
+         
+         // matriz confusão
          double[][] cmMatrix = eTest.confusionMatrix();
          for(int row_i=0; row_i<cmMatrix.length; row_i++){
              for(int col_i=0; col_i<cmMatrix.length; col_i++){
-                 System.out.print(cmMatrix[row_i][col_i]);
-                 System.out.print("|");
+            	 writer.print(cmMatrix[row_i][col_i]);
+            	 writer.print("|");
              }
-             System.out.println();
+             writer.println();
          }
+         writer.close();
     }
 }
